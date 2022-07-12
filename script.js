438345a924ec5f1f2100353fca86d8ec82d5f9de
Emily <emily.kristine.hart@gmail.com>
2022-07-11 20:50:12 -0500

    hopefully works!

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
 
 
 window.addEventListener("load", function() {
-    document.addEventListener("submit", function (event){
+    myFetch();
+    // const planets = myFetch();
+    // pickPlanet(planets);
+
+           let listedPlanets;
+           // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+           let listedPlanetsResponse = myFetch();
+           listedPlanetsResponse.then(function (result) {
+               listedPlanets = result;
+            //    console.log(listedPlanets);
+           }).then(function () {
+    //    console.log(listedPlanets);
+       luckyPlanet = pickPlanet(listedPlanets);
+       console.log(luckyPlanet);
+    const name = luckyPlanet.name;
+    const diameter = luckyPlanet.diameter;
+    const star = luckyPlanet.star;
+    const distance = luckyPlanet.distance;
+    const moons = luckyPlanet.moons;
+    const imageUrl = luckyPlanet.image;
+    addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl);
+    })
+
+    // const name = luckyPlanet.name;
+    // const diameter = luckyPlanet.diameter;
+    // const star = luckyPlanet.star;
+    // const distance = luckyPlanet.distance;
+    // const moons = luckyPlanet.moons;
+    // const imageUrl = luckyPlanet.image;
+    // addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl);
+       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+    
+        document.addEventListener("submit", function (event){
         event.preventDefault();
-        
+
         const list = document.getElementById("faultyItems").value;
         const pilot = document.querySelector("[name=pilotName]").value;
         const copilot = document.querySelector("[name=copilotName]").value;
@@ -11,18 +43,6 @@ window.addEventListener("load", function() {
         const cargoMass = document.querySelector("[name=cargoMass]").value;
         
         formSubmission(document, list, pilot, copilot, fuelLevel, cargoMass);
-
-           let listedPlanets;
-           // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-           let listedPlanetsResponse;
-           listedPlanetsResponse.then(function (result) {
-               listedPlanets = result;
-               console.log(listedPlanets);
-           }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-    
     });
 });
 
--- scriptHelper.js
+++ scriptHelper.js
@@ -3,17 +3,18 @@
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
    // Here is the HTML formatting for our mission target div.
-   `
+   const destinationInfo = document.getElementById("missionTarget");
+   destinationInfo.innerHTML =`
                 <h2>Mission Destination</h2>
                 <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
+                    <li>Name:${name} </li>
+                    <li>Diameter: ${diameter} </li>
                     <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
+                    <li>Distance from Earth: ${distance} </li>
+                    <li>Number of Moons: ${moons} </li>
                 </ol>
-                <img src="">
-   `
+                <img src="${imageUrl}">
+   `;
 }
 function validateInput(testInput) {   
         if (testInput==="") {
@@ -96,13 +97,16 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoMass) {
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+        return response.json(); 
         });
-
     return planetsReturned;
 }
 
 function pickPlanet(planets) {
+    let index = myFetch.name;
+    let pickedPlanets = Math.floor(Math.random() * (index.length));
+    return planets[pickedPlanets];
 }
 
 // module.exports = {
